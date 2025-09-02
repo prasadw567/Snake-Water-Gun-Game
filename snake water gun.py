8d22e4756cd5057a19c073a2e4f3ce29cb2b03c2
Prasad Wadkar <prasadw567@gmail.com>
2025-09-02 14:22:09 +0530

    Update snake water gun.py

--- snake water gun.py
+++ snake water gun.py
@@ -2,11 +2,11 @@ elements=["snake","water","gun"]
 
 import random
 
-choice_comp=(random.choice(elements))
 print("Welcome to the Game","\nSNAKE,WATER,GUN")
 input("Press Enter to start the game!!!")
 
 while True:
+    choice_comp=(random.choice(elements))
     print("\n1:snake","2:water","3:gun")
     a=int(input("Enter your choice:"))
     user_choice=elements[a-1]
@@ -50,3 +50,4 @@ while True:
     play_again=input("Do you want to play again(y/n):")
     if(play_again=="n"):
         break
+
